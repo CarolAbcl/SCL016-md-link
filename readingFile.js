6aeed29de7b28462f1c2a9800bd3272c7796eace
CarolAbcl <carolallende@gmail.com>
2021-05-18 12:49:56 -0400

    array with links ready

--- readingFile.js
+++ readingFile.js
@@ -1,3 +1,5 @@
+const { link } = require('fs');
+
 fs = require('fs');
 marked = require('marked');
 module.exports = (route) => {
@@ -6,49 +8,20 @@ module.exports = (route) => {
     if (err) {
       console.log('error: ', err);
     } else {
-      console.log(data);
-      console.log(marked(data))
-      /*const expresion = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/;
-      let foundLinks = data.match(expresion);
-      console.log(foundLinks);*/
+      const arrayLinks= []
+      let render = new marked.Renderer();
+      render.link = function (href, title, text) {
+        const linkElements = {
+          href,
+          text,
+          file: myFile,
+        };
+        arrayLinks.push(linkElements);
+      };
+      marked(data,{
+        renderer: render
+      })
+      console.log(arrayLinks);
     }
   });
 } 
-
-const foundLinks = (route, data) => {
-  const arrayLinks = [];
-  const renderer = new marked.Renderer(); // customizar dependiendo de la sintaxis
-  searchRoutemd(route).forEach((data) => {
-    renderer.link = (href, title, text) => { // renderer define salida ouput con tres propiedades
-      const linkProperties = {
-        href,
-        text,
-        file,
-      };
-      arrayLinks.push(linkProperties);
-    };
-    marked(readFilePath(file), { renderer });
-    console.log(arrayLinks);
-  });
-  const arrayLinkFilter = arrayLinks.filter((element) => /^(https?:\/\/)/.test(element.href));
-  return arrayLinkFilter;
-};
-
-/*const regularExpressions = (data) => {
-  console.log(typeof data);
-  const anyLink = data;
-  const regularExp = /(http:\/\/)?(www\.)[a-zA-Z0-9.-]+\.(com|net|cl)/;
-      let position = anyLink.indexOf('http');
-      console.log(position);
-      if (regularExp.test(anyLink)){
-          console.log('es una URL');
-          return true;
-      }else{
-          console.log('NO es una URL');
-          return false;
-      } 
-      
-  }
-  
-  //regularExpressions();
-*/
